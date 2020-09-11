03cb3f2346348632301ca4d3d3eca555060e0a44
Dario Carlino <baires82@gmail.com>
2020-09-11 01:12:23 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
+  @@know = []
 
+  def initialize
+    @knowledge = @@know
+  end
+
+  def learn(string_of_knowledge)
+    @@know << string_of_knowledge
+  end
+
+  def knowledge
+    @@know
+  end
 end
