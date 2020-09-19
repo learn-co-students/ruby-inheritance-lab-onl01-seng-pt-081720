20e27f1f759834cd09a60bb490b539493b553ab7
Tim Flood <tflood9@gmail.com>
2020-09-19 16:53:26 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(thing_to_learn)
+    @knowledge << thing_to_learn
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
